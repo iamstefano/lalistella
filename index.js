5c091147f658b77c240a2a25fd6fb5e096023155
Iamstefanoe <stefanoar3@gmail.com>
2023-10-09 21:48:03 +0200

    feature: switchable color themes

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
-import "./style.css";
+import "./style.scss";
 
+// t h e m e
+const colorThemes = document.querySelectorAll('[name="theme"]');
+
+// store theme
+const storeTheme = function (theme) {
+  localStorage.setItem("theme", theme);
+};
+
+// set theme when visitor returns
+const setTheme = function () {
+  const activeTheme = localStorage.getItem("theme");
+  colorThemes.forEach((themeOption) => {
+    if (themeOption.id === activeTheme) {
+      themeOption.checked = true;
+    }
+  });
+  // fallback for no :has() support
+  document.documentElement.className = activeTheme;
+};
+
+colorThemes.forEach((themeOption) => {
+  themeOption.addEventListener("click", () => {
+    storeTheme(themeOption.id);
+    // fallback for no :has() support
+    document.documentElement.className = themeOption.id;
+  });
+});
+
+document.onload = setTheme();
+
+// f i r e b a s e
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
 
 import {
@@ -14,7 +45,7 @@ const appSettings = {
 };
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
+/* const firebaseConfig = {
   apiKey: "AIzaSyDQ7bZmt5Z6mururrMcwuUR2Eby8cQ06lY",
   authDomain: "lecosedacomprare-511f2.firebaseapp.com",
   databaseURL:
@@ -24,9 +55,9 @@ const firebaseConfig = {
   messagingSenderId: "43074085832",
   appId: "1:43074085832:web:617b613c6bf4f28ed356fd",
 };
-
+ */
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+/* const app = initializeApp(firebaseConfig);
 
 const app = initializeApp(appSettings);
 const database = getDatabase(app);
@@ -39,4 +70,4 @@ addButtonEl.addEventListener("click", function () {
   let inputValue = inputFieldEl.value;
 
   console.log(inputValue);
-});
+}); */
